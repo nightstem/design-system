2a1804592779aaadaa9a4cbc34e7e3b2b6a352e1
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2026-06-07 22:55:27 -0600

    Upgrade packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig([
       '**/node_modules/**',
       '**/dist/**',
       '**/coverage/**',
+      '**/storybook-static/**',
       '**/*.d.ts',
       'pnpm-lock.yaml',
     ],
